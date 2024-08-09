580cfe1ec207c18b7435efb71dd9d591bd526966
Nicole Malaquias <nicole.malaquias@indicium.tech>
2024-08-08 21:59:29 -0300

    style(staging): made adjustment on the style code

--- models/staging/stg_sales_order_header.sql
+++ models/staging/stg_sales_order_header.sql
@@ -1,29 +1,28 @@
 with 
     source as (
     select 
+        -- Primary Key
+        salesorderid
 
-            -- Primary Key
-            salesorderid
+        -- Foreign Key
+        , creditcardid
+        , customerid
+        , territoryid
+        , billtoaddressid
+        , shiptoaddressid
+        , shipmethodid
+        , currencyrateid
 
-            -- Foreign Key
-            , creditcardid
-            , customerid
-            , territoryid
-            , billtoaddressid
-            , shiptoaddressid
-            , shipmethodid
-            , currencyrateid
-
-            , cast(orderdate as timestamp) as orderdate
-            , cast(duedate as timestamp) as duedate
-            , cast(shipdate as timestamp) as shipdate
-            , status
-            , onlineorderflag
-            , accountnumber
-            , creditcardapprovalcode
-            , subtotal
-            , taxamt   
-
+        -- Stich Columns
+        , cast(orderdate as timestamp) as orderdate
+        , cast(duedate as timestamp) as duedate
+        , cast(shipdate as timestamp) as shipdate
+        , status
+        , onlineorderflag
+        , accountnumber
+        , creditcardapprovalcode
+        , subtotal
+        , taxamt   
     from {{ source('adventure_works', 'salesorderheader') }}
     )   
 
